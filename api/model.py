d666d2eea29d66559e98cae9da9f902da410d482
najeeb67 <149230449+najeeb67@users.noreply.github.com>
2024-06-15 17:34:16 +0500

    Create DataBase And Create engine of Project

---
+++ api/model.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+class Doctor(Base):
+    __tablename__ = "doctors"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    specialization = Column(String)
+    patients = relationship("Patient", back_populates="doctor")
+
+class Patient(Base):
+    __tablename__ = "patients"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    age = Column(Integer)
+    doctor_id = Column(Integer, ForeignKey("doctors.id"))
+    doctor = relationship("Doctor", back_populates="patients")
---
+++ api/schemas.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
+
+class DoctorBase(BaseModel):
+    name: str
+    specialization: str
+    time_in: Optional[datetime] = None
+    time_out: Optional[datetime] = None
+
+class DoctorCreate(DoctorBase):
+    pass
+
+class DoctorUpdate(DoctorBase):
+    pass
+
+class Doctor(DoctorBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class PatientBase(BaseModel):
+    name: str
+    age: int
+    admit_date: Optional[datetime] = None
+    discharge_date: Optional[datetime] = None
+    doctor_id: int
+
+class PatientCreate(PatientBase):
+    pass
+
+class PatientUpdate(PatientBase):
+    pass
+
+class Patient(PatientBase):
+    id: int
+
+    class Config:
+        orm_mode = True
